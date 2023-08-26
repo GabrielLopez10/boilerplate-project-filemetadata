0fc7ff54b18defd0932e63cf13ddf1933620535f
Replit user <>
2023-08-26 16:02:07 -0300

    files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -7,11 +8,26 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  const metadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+
+  res.json(metadata);
+});
 
 
 const port = process.env.PORT || 3000;
